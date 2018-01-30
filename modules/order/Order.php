fd0aaf303b81e1b589c3516f3897930fdf5c0d68
vdekh <vicdekh@gmail.com>
2018-01-30 21:34:25 +0300

    Create module Order

---
+++ modules/order/Order.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\order;
+
+/**
+ * order module definition class
+ */
+class Order extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modeules\order\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
